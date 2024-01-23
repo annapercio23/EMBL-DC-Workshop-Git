4369009f3970dffaf0f11868ad68fa2c508e4cf7
Anna Percio <annapercio@gmail.com>
2024-01-23 15:57:14 +0100

    scritture

--- scripts/my first code.R
+++ scripts/my first code.R
@@ -19,4 +19,26 @@ weight_lb <-  2.2 * my_weight_kg
 weight_kg <-  100
 
 weight_lb <-  2.2 * weight_kg
+# I have to update value of the weight_kg object, be verbose!!!
 
+mass <- 47.5
+age <-  122
+mass <- mass * 2
+age <- age - 20
+mass_index <-  mass/age
+sqrt(49)
+myweight_kg <- sqrt(9) 
+round(4675.98675, 2)
+my_number <- round(4675.98675, 2)
+round(digits = 3, x = 19.9999999)
+round(digits = 3, x = 19.56789)
+
+my_function <- function(x){
+  x = x + 2
+}
+
+my_result <- my_function(3)
+
+weight_kg <- c(40, 50, 60, 70)
+mean(weight_kg [1:4])
+animals <- c("mouse","duck", "dog")
