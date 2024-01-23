cfdfd92ec6e5f10f80cc156ab0a0615b11baa6bb
Anna Percio <annapercio@gmail.com>
2024-01-23 15:01:30 +0100

    ciaoo

---
+++ scripts/my first code.R
@@ -0,0 +1,22 @@
+weight_kg <- 55
+
+Weight_Kg <- 45
+
+my_weight_kg <- 60
+
+(my_weight_kg <- 50)
+(my_weight_kg <- 60)
+my_weight_kg
+
+2.2 * my_weight_kg
+weight_kg <- 62
+2.2 + my_weight_kg
+2.2 * my_weight_kg
+my_weight_kg <- 65
+2.2 * my_weight_kg
+
+weight_lb <-  2.2 * my_weight_kg
+weight_kg <-  100
+
+weight_lb <-  2.2 * weight_kg
+
