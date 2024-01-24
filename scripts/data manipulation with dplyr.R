43862a70b93043503bb6cdead830e226b389f909
Anna Percio <annapercio@gmail.com>
2024-01-24 12:32:06 +0100

    dplyr

--- scripts/data manipulation with dplyr.R
+++ scripts/data manipulation with dplyr.R
@@ -40,11 +40,83 @@ surveys %>%
 surveys %>% 
   mutate(weight_kg = weight/1000, weight_lb = weight_kg * 2.2) %>% 
   view()
-#alternativamente si puo anche usare head per vedere prime sei righe
+#alternativamente si può anche usare head per vedere prime sei righe 
+#o tail ultime
 #filtra togliendo i missing del peso
 surveys %>% 
   filter(!is.na(weight)) %>% 
   mutate(weight_kg = weight/1000, weight_lb = weight_kg * 2.2) %>% 
   view()
+
+#confrontiamo il peso dei maschi con il peso delle femmine, 
+#praticamente è come se si diviesse la tabella
+#per gruppi di interesse
+#nb l'obiettivo finale è sull'ultima riga
+surveys%>% 
+  group_by(sex) %>% 
+  summarise(mean_weight = mean(weight, na.rm=TRUE))
+
+#remove na from sex
+surveys%>% 
+  group_by(sex) %>% 
+  filter(!is.na(sex)) %>% 
+  summarise(mean_weight = mean(weight, na.rm=TRUE))
+ #per creare tabella 
+# tabella <- surveys%>% 
+#   group_by(sex) %>% 
+#   filter(!is.na(sex)) %>% 
+#   summarise(mean_weight = mean(weight, na.rm=TRUE))
+
+table5 <- surveys%>% 
+  group_by(sex, species_id) %>% 
+  filter(!is.na(sex), !is.na(species_id)) %>% 
+  summarise(mean_weight = mean(weight, na.rm=TRUE)) %>% 
+  print(n=15)
+#usando la funzione print sipuò vedere il numero di righe che vogliamo 
+#specificando il numero n=
+surveys%>% 
+  group_by(sex, species_id) %>% 
+  filter(!is.na(sex), !is.na(species_id), !is.na(weight)) %>% 
+  summarise(mean_weight = mean(weight, na.rm=TRUE), 
+            min_weight=min(weight)) %>% 
+  arrange(min_weight)
+#arrange per ordinarlo in ordine crescente in base a colonna x
+#in ordine dercrescente arrange(desc(min_weight))
+surveys%>% 
+  group_by(sex, species_id) %>% 
+  filter(!is.na(sex), !is.na(species_id), !is.na(weight)) %>% 
+  summarise(mean_weight = mean(weight, na.rm=TRUE), 
+            min_weight=min(weight)) %>% 
+  arrange(desc(min_weight))
+
+#count per avere il nuemro di osservazioni
+surveys%>% 
+  count(sex)
+
+surveys%>% 
+  count(sex, species) %>% 
+  arrange(species,desc(n))
+#desc (n) per l'ordine descrescente del numero di osservazioni
+
+#challenge
+animalsinplot <- surveys%>% 
+  count(species, plot_type)
+
+animalscharac <-  surveys%>% 
+  group_by(species_id) %>% 
+  filter(!is.na(hindfoot_length)) %>% 
+  summarise(mean_lenght = mean(hindfoot_length), min_lenght 
+            = min(hindfoot_length), max_lenght= max(hindfoot_length))
+
+heaviestperyear <- surveys%>% 
+  filter((!is.na(weight))) %>% 
+   group_by(year) %>% 
+  filter(weight == max(weight)) %>% 
+  select(year, genus, species_id, weight) %>% 
+  arrange(desc(year)) %>% 
+  unique()
+#  unique() per eliminare i duplicati
+
+
 
 
